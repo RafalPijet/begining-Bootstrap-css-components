821d1ca0f88fd313c3c65ca68e858f87e141fd91
Rafal Pijet <stronglopez@wp.pl>
2019-08-11 14:32:55 +0200

    Add next examples

--- script.js
+++ script.js
@@ -3,6 +3,12 @@
     const box1Header = document.querySelector("#box1 header");
     const box3 = document.getElementById("box3");
     const box4 = document.getElementById("box4");
+    const box5 = document.getElementById("box5");
+    const box6 = document.getElementById("box6");
+    const box7 = document.getElementById("box7");
+    const box8 = document.getElementById("box8");
+    const box9 = document.getElementById("box9");
+    const box10 = document.getElementById("box10");
     const box2Header = document.querySelector("#box2 header");
     const secondHeader = document.getElementById("box-header");
     const ourBox = secondHeader.parentNode;
@@ -37,6 +43,48 @@
         box4.querySelector("header").classList.toggle("header-special");
     };
 
+    const eventCallbackForAllBoxes = function (event) {
+        this.querySelector("header").insertAdjacentHTML("afterEnd", "Detected event " + event.type + "<br>");
+    };
+
+    const headerClickCallback = function () {
+      this.parentElement.classList.toggle("header-special");
+    };
+
     box4.addEventListener("mouseenter", boxEventCallbackBefore);
     box4.addEventListener("mouseleave", boxEventCallbackBefore);
+    box5.addEventListener("mouseenter", eventCallbackForAllBoxes);
+    box5.addEventListener("mouseleave", eventCallbackForAllBoxes);
+    box6.addEventListener("mouseenter", eventCallbackForAllBoxes);
+    box6.addEventListener("mouseleave", eventCallbackForAllBoxes);
+    box7.addEventListener("mouseenter", eventCallbackForAllBoxes);
+    box7.addEventListener("mouseleave", eventCallbackForAllBoxes);
+    box5.querySelector("header").addEventListener("click", headerClickCallback);
+    box6.querySelector("header").addEventListener("click", headerClickCallback);
+    box7.querySelector("header").addEventListener("click", headerClickCallback);
+
+    const headerClickAndChangeStyle = function () {
+        this.classList.toggle("header-special");
+    };
+
+    const boxClickCallback = function () {
+        this.classList.toggle("box-bg");
+    };
+    box8.addEventListener("click", boxClickCallback);
+    box9.addEventListener("click", boxClickCallback);
+    box10.addEventListener("click", boxClickCallback);
+    box8.querySelector("header").addEventListener("click", headerClickAndChangeStyle);
+    box9.querySelector("header").addEventListener("click", headerClickAndChangeStyle);
+    box10.querySelector("header").addEventListener("click", headerClickAndChangeStyle);
+    box10.querySelector("header").addEventListener("click", function (event) {
+        event.stopPropagation();
+    });
+
+    const box10Link = document.querySelector(".toggle-special-header");
+    box10Link.addEventListener("click", function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        this.parentElement.querySelector("header").classList.toggle("header-special");
+    })
+
 })();
